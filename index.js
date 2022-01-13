133b212867bdce697bc67bc1a27eaa90d679a236
Manzaraz <magnusmanz@gmail.com>
2022-01-12 22:05:09 -0300

    adding endpoints

--- index.js
+++ index.js
@@ -31,6 +31,47 @@ app.get("/products/:productId", async (req, res) => {
     res.json(error);
   }
 });
-git;
+// Get Product  Reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+// Get Product  Offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+// Get Search Results
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
